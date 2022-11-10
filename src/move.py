293659e18308122dc8a7f123da2848c46f12ee00
Aizaixyq <515139800@qq.com96584640+Aizaixyq@users.noreply.github.com>
2022-11-11 00:09:11 +0800

    fix(src/move.py) add move_direction

--- src/move.py
+++ src/move.py
@@ -15,24 +15,33 @@ IN4 = 23
 ENA = 13
 ENB = 22
 
+dir = ["go":[1, 1], "back":[0, 0], "right":[1, 0], "left":[0, 1]]
+
 # 设置引脚为输出
 pin_arr = [IN1, IN2, IN3, IN4, ENA, ENB];
 for pin in pin_arr:
     GPIO.setup(pin, GPIO.OUT)
 
+#创建实例
 pwm_ENB = GPIO.PWM(ENB,500)
 pwm_ENA = GPIO.PWM(ENA,500)
 #pwm启动
 pwm_ENA.start(0)
 pwm_ENB.start(0)
 
-def forward():
-    GPIO.output(IN1, False)     # 将IN1设置为0
-    GPIO.output(IN2, True)      # 将IN2设置为1
-    #GPIO.output(ENA, True)      # 将ENA设置为1，启动A通道电机
-    GPIO.output(IN3, False)     # 将IN3设置为0
-    GPIO.output(IN4, True)      # 将IN4设置为1
-    #GPIO.output(ENB, True)      # 将ENB设置为1，启动B通道电机
+def move_direction(state_box: List[bool]) -> None:
+
+    Lad1, Lad2, Rad3, Rad4 = True, False, True, False
+    if state_box[0]:#左轮是否前进
+        Lad1 = False
+        Lad2 = True
+    if state_box[1]:#右轮是否前进
+        Rad3 = False
+        Rad4 = True
+    GPIO.output(IN1, Lad1)     # 将IN1设置为0
+    GPIO.output(IN2, Lad2)      # 将IN2设置为1
+    GPIO.output(IN3, Rad3)     # 将IN3设置为0
+    GPIO.output(IN4, Rad4)      # 将IN4设置为1
     pwm_ENB.ChangeDutyCycle(20)
     pwm_ENA.ChangeDutyCycle(20)
 
@@ -43,6 +52,7 @@ def stop():
 
 
 def ctrl_server():
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('192.168.3.18', 8080))
     server.listen(5)
@@ -50,12 +60,10 @@ def ctrl_server():
     print('Accept new connection from %s:%s...' % addr)
     while True:
         data = sock.recv(1024)
-        if not data or data.decode('utf-8') == 'exit':
+        if not data or data == 'exit':
             break
-        elif data.decode('utf-8') == 'go':
-            forward()
-        elif data.decode('utf-8') == 'stop':
-            stop()
+        elif data in dir:
+            move_direction(dir[data])
     sock.send(b"disconnect")
     sock.close()
     print("disconnect")
