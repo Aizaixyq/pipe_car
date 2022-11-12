d53e5aea41b1fe94af20e31df3ccbe8ed27b98d1
Aizaixyq <515139800@qq.com96584640+Aizaixyq@users.noreply.github.com>
2022-11-12 14:58:06 +0800

    fix(lua) add config open gtk

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,8 @@
-add_requires("opencv", "fmt", "ffmpeg")
+add_requires("fmt")
+add_requires("opencv", {configs = {gtk = true}})
 
 target("video")
     set_kind("binary")
     set_languages("c++20")
     add_files("src/*.cpp")
-    add_packages("opencv", "fmt", "ffmpeg")
+    add_packages("opencv", "fmt")
